df5cab849998283a1552dc204dc15091969dda1d
이상우 <soidoux@isang-uui-MacBookAir.local>
2024-03-25 16:52:26 +0900

    week6

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const emailInput = document.querySelector('#email-input');
-const emailErrorMessage = document.querySelector('.emailerrormessage');
-const passwordInput = document.querySelector('#password-input')
-const passwordErrorMessage = document.querySelector('.passworderrormessage');
-const loginButton = document.querySelector('.button');
-const passwordConfirmInput = document.querySelector('#passwordconfirm-input')
-const passwordConfirmErrorMessage = document.querySelector('.passwordconfirmerrormessage')
-const passwordEyeButton = document.querySelector('#password-eye-button');
-
+const emailInput = document.querySelector("#email-input");
+const emailErrorMessage = document.querySelector(".emailerrormessage");
+const passwordInput = document.querySelector("#password-input");
+const passwordErrorMessage = document.querySelector(".passworderrormessage");
+const loginButton = document.querySelector(".button");
+const passwordConfirmInput = document.querySelector("#passwordconfirm-input");
+const passwordConfirmErrorMessage = document.querySelector(
+  ".passwordconfirmerrormessage"
+);
+const passwordEyeButton = document.querySelector("#password-eye-button");
 
 function isEmailValid(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -15,41 +16,45 @@ function isEmailValid(email) {
 }
 
 function addInputError(inp, inpErr, errMsg) {
-  inp.classList.add('sign-input-error');
-  inpErr.classList.add('error-message-on');
+  inp.classList.add("sign-input-error");
+  inpErr.classList.add("error-message-on");
   inpErr.textContent = errMsg;
 }
 
 function removeInputError(inp, inpErr) {
-  inp.classList.remove('sign-input-error');
-  inpErr.classList.remove('error-message-on');
+  inp.classList.remove("sign-input-error");
+  inpErr.classList.remove("error-message-on");
   inpErr.textContent = "";
 }
 
-
 function validateEmailInput(event) {
-
   const email = emailInput.value;
 
-  if(email === "") {
+  if (email === "") {
     addInputError(emailInput, emailErrorMessage, "이메일을 입력해주세요.");
     return;
   }
-  if(!isEmailValid(email)) {
-    addInputError(emailInput, emailErrorMessage, "올바른 이메일 주소가 아닙니다.");
+  if (!isEmailValid(email)) {
+    addInputError(
+      emailInput,
+      emailErrorMessage,
+      "올바른 이메일 주소가 아닙니다."
+    );
     return;
   }
 
   removeInputError(emailInput, emailErrorMessage);
-  
 }
 
 function validatePasswordInput(event) {
-
   const password = passwordInput.value;
 
-  if(password === "") {
-    addInputError(passwordInput, passwordErrorMessage, "비밀번호를 입력해주세요.");
+  if (password === "") {
+    addInputError(
+      passwordInput,
+      passwordErrorMessage,
+      "비밀번호를 입력해주세요."
+    );
     return;
   }
 
@@ -60,37 +65,33 @@ function confirmPasswordInput(event) {
   const password = passwordInput.value;
   const passwordConfirm = passwordConfirmInput.value;
 
-
-  if(password !== passwordConfirm) {
-    addInputError(passwordConfirmInput, passwordConfirmErrorMessage, "비밀번호가 일치하지 않아요.");
+  if (password !== passwordConfirm) {
+    addInputError(
+      passwordConfirmInput,
+      passwordConfirmErrorMessage,
+      "비밀번호가 일치하지 않아요."
+    );
     return;
   }
 
   removeInputError(passwordConfirmInput, passwordConfirmErrorMessage);
 }
 
+emailInput.addEventListener("focusout", validateEmailInput);
 
-
-emailInput.addEventListener('focusout', validateEmailInput);
-
-passwordInput.addEventListener('focusout', validatePasswordInput);
-
-
+passwordInput.addEventListener("focusout", validatePasswordInput);
 
 function changeEyeButton(event) {
-  if(passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    passwordEyeButton.querySelector('img').src = 'imgfolder/eye-on.svg';
-  }
-  else {
-    passwordInput.type = 'password';
-    passwordEyeButton.querySelector('img').src = 'imgfolder/eye-off.svg';
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordEyeButton.querySelector("img").src = "imgfolder/eye-on.svg";
+  } else {
+    passwordInput.type = "password";
+    passwordEyeButton.querySelector("img").src = "imgfolder/eye-off.svg";
   }
 }
 
-passwordEyeButton.addEventListener('click', changeEyeButton);
-
-
+passwordEyeButton.addEventListener("click", changeEyeButton);
 
 function loginUser() {
   const email = emailInput.value;
@@ -102,15 +103,40 @@ function loginUser() {
   if (email === validEmail && password === validPassword) {
     window.location.href = "folder.html";
     return;
-  }
-  else {
+  } else {
     addInputError(emailInput, emailErrorMessage, "이메일을 확인해주세요.");
 
-    addInputError(passwordInput, passwordErrorMessage, "비밀번호를 확인해주세요.");
+    addInputError(
+      passwordInput,
+      passwordErrorMessage,
+      "비밀번호를 확인해주세요."
+    );
     return;
   }
 }
 
+const signForm = document.querySelector("#form");
+signForm.addEventListener("submit", submitForm);
+async function submitForm(event) {
+  event.preventDefault();
 
-
-
+  try {
+    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
+      method: "POST",
+      body: JSON.stringify({
+        email: emailInput.value,
+        password: passwordInput.value,
+      }),
+    });
+    //아이디와 비밀번호를 요청해서 응답을 받는 법 모르겠어요
+  } catch {
+    setInputError(
+      { input: emailInput, errorMessage: emailErrorMessage },
+      "이메일을 확인해주세요."
+    );
+    setInputError(
+      { input: passwordInput, errorMessage: passwordErrorMessage },
+      "비밀번호를 확인해주세요."
+    );
+  }
+}
--- signup.js
+++ signup.js
@@ -1,13 +1,16 @@
-const emailInput = document.querySelector('#email-input');
-const emailErrorMessage = document.querySelector('.emailerrormessage');
-const passwordInput = document.querySelector('#password-input')
-const passwordErrorMessage = document.querySelector('.passworderrormessage');
-const signButton = document.querySelector('.button');
-const passwordConfirmInput = document.querySelector('#password-confirm-input')
-const passwordConfirmErrorMessage = document.querySelector('.passwordconfirmerrormessage')
-const passwordEyeButton = document.querySelector('#password-eye-button');
-const passwordConfirmEyeButton = document.querySelector('#password-confirm-eye-button');
-
+const emailInput = document.querySelector("#email-input");
+const emailErrorMessage = document.querySelector(".emailerrormessage");
+const passwordInput = document.querySelector("#password-input");
+const passwordErrorMessage = document.querySelector(".passworderrormessage");
+const signButton = document.querySelector(".button");
+const passwordConfirmInput = document.querySelector("#password-confirm-input");
+const passwordConfirmErrorMessage = document.querySelector(
+  ".passwordconfirmerrormessage"
+);
+const passwordEyeButton = document.querySelector("#password-eye-button");
+const passwordConfirmEyeButton = document.querySelector(
+  "#password-confirm-eye-button"
+);
 
 function isEmailValid(email) {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -16,49 +19,60 @@ function isEmailValid(email) {
 }
 
 function addInputError(inp, inpErr, errMsg) {
-  inp.classList.add('sign-input-error');
-  inpErr.classList.add('error-message-on');
+  inp.classList.add("sign-input-error");
+  inpErr.classList.add("error-message-on");
   inpErr.textContent = errMsg;
 }
 
 function removeInputError(inp, inpErr) {
-  inp.classList.remove('sign-input-error');
-  inpErr.classList.remove('error-message-on');
+  inp.classList.remove("sign-input-error");
+  inpErr.classList.remove("error-message-on");
   inpErr.textContent = "";
 }
 
-
 function validateEmailInput(event) {
-
   const email = emailInput.value;
 
-  if(email === "") {
+  if (email === "") {
     addInputError(emailInput, emailErrorMessage, "이메일을 입력해주세요.");
     return;
   }
-  if(!isEmailValid(email)) {
-    addInputError(emailInput, emailErrorMessage, "올바른 이메일 주소가 아닙니다.");
+  if (!isEmailValid(email)) {
+    addInputError(
+      emailInput,
+      emailErrorMessage,
+      "올바른 이메일 주소가 아닙니다."
+    );
     return;
   }
-  if(email === "test@codeit.com") {
+  if (email === "test@codeit.com") {
     addInputError(emailInput, emailErrorMessage, "이미 사용중인 이메일입니다.");
     return;
   }
 
   removeInputError(emailInput, emailErrorMessage);
-  
 }
 
 function validatePasswordInput(event) {
-
   const password = passwordInput.value;
 
-  if(password === "") {
-    addInputError(passwordInput, passwordErrorMessage, "비밀번호를 입력해주세요.");
+  if (password === "") {
+    addInputError(
+      passwordInput,
+      passwordErrorMessage,
+      "비밀번호를 입력해주세요."
+    );
     return;
   }
-  if(password.length<8 || (/[a-zA-Z]/.test(password) && /[0-9]/.test(password))===false) {
-    addInputError(passwordInput, passwordErrorMessage, "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.");
+  if (
+    password.length < 8 ||
+    (/[a-zA-Z]/.test(password) && /[0-9]/.test(password)) === false
+  ) {
+    addInputError(
+      passwordInput,
+      passwordErrorMessage,
+      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+    );
     return;
   }
 
@@ -69,60 +83,62 @@ function confirmPasswordInput(event) {
   const password = passwordInput.value;
   const passwordConfirm = passwordConfirmInput.value;
 
-
-  if(password !== passwordConfirm) {
-    addInputError(passwordConfirmInput, passwordConfirmErrorMessage, "비밀번호가 일치하지 않아요.");
+  if (password !== passwordConfirm) {
+    addInputError(
+      passwordConfirmInput,
+      passwordConfirmErrorMessage,
+      "비밀번호가 일치하지 않아요."
+    );
     return;
   }
 
   removeInputError(passwordConfirmInput, passwordConfirmErrorMessage);
 }
 
+emailInput.addEventListener("focusout", validateEmailInput);
 
+passwordInput.addEventListener("focusout", validatePasswordInput);
 
-emailInput.addEventListener('focusout', validateEmailInput);
-
-passwordInput.addEventListener('focusout', validatePasswordInput);
-
-passwordConfirmInput.addEventListener('focusout', confirmPasswordInput);
-
-
+passwordConfirmInput.addEventListener("focusout", confirmPasswordInput);
 
 function changePasswordEyeButton(event) {
-  if(passwordInput.type === 'password') {
-    passwordInput.type = 'text';
-    passwordEyeButton.querySelector('img').src = 'imgfolder/eye-on.svg';
-  }
-  else {
-    passwordInput.type = 'password';
-    passwordEyeButton.querySelector('img').src = 'imgfolder/eye-off.svg';
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordEyeButton.querySelector("img").src = "imgfolder/eye-on.svg";
+  } else {
+    passwordInput.type = "password";
+    passwordEyeButton.querySelector("img").src = "imgfolder/eye-off.svg";
   }
 }
 
-passwordEyeButton.addEventListener('click', changePasswordEyeButton);
-
+passwordEyeButton.addEventListener("click", changePasswordEyeButton);
 
 function changePasswordConfirmEyeButton(event) {
-  if(passwordConfirmInput.type === 'password') {
-    passwordConfirmInput.type = 'text';
-    passwordConfirmEyeButton.querySelector('img').src = 'imgfolder/eye-on.svg';
-  }
-  else {
-    passwordConfirmInput.type = 'password';
-    passwordConfirmEyeButton.querySelector('img').src = 'imgfolder/eye-off.svg';
+  if (passwordConfirmInput.type === "password") {
+    passwordConfirmInput.type = "text";
+    passwordConfirmEyeButton.querySelector("img").src = "imgfolder/eye-on.svg";
+  } else {
+    passwordConfirmInput.type = "password";
+    passwordConfirmEyeButton.querySelector("img").src = "imgfolder/eye-off.svg";
   }
 }
 
-passwordConfirmEyeButton.addEventListener('click', changePasswordConfirmEyeButton);
-
-
+passwordConfirmEyeButton.addEventListener(
+  "click",
+  changePasswordConfirmEyeButton
+);
 
 function signupUser() {
-  const inputErrorElement = document.querySelector('.sign-input-error');
-  if(inputErrorElement) {
-
+  const inputErrorElement = document.querySelector(".sign-input-error");
+  if (inputErrorElement) {
     return;
   }
 
   window.location.href = "folder.html";
+}
+
+function Obj() {
+  const obj1 = { a: 1, b: 2 };
+  const obj2 = { b: 3, c: 4 };
+  console.log({ ...obj1, ...obj2 });
 }
